d92a889d8dd29b9b31f666fb4ca3a26ae99b4b91
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-08 02:33:22 -0300

    update. add Routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {StatusBar} from 'react-native';
 import Routes from './routes';
 
-const App= () => <Routes />
+const App= () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#000000"/>
+    <Routes />
+  </>
+);
 
 export default App;
